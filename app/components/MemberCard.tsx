3f99cbdebd4cc35b3cefddbf0d1112a87a848c24
jujudev7 <penna.julien@gmail.com>
2024-11-24 21:37:00 +0100

    Member & Premium Cards modified

--- app/components/MemberCard.tsx
+++ app/components/MemberCard.tsx
@@ -1,3 +1,5 @@
+import { createSubscription } from "@/lib/actionsStripe";
+import { SignOutButton } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
@@ -9,22 +11,27 @@ export default async function MemberCard() {
     redirect("/");
   }
   return (
-    <div className="w-full h-screen flex items-center justify-center flex-col">
+    <section className="w-full h-screen flex items-center justify-center flex-col">
       <Image
         src={user?.imageUrl as string}
         width={150}
         height={150}
         alt=""
         className="rounded-full mb-2"
+        priority
       />
       <h1>Bienvenue {user?.fullName}</h1>
       <p>Email : {user?.emailAddresses[0].emailAddress}</p>
 
-      <form className="my-4" action="">
+      <form className="my-4" action={createSubscription}>
         <button className="p-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full">
           Devenir membre Premium
         </button>
       </form>
-    </div>
+
+      <div className="p-2 rounded-md bg-red-500 hover:bg-red-600 text-white">
+        <SignOutButton />
+      </div>
+    </section>
   );
 }
--- app/components/PremiumCard.tsx
+++ app/components/PremiumCard.tsx
@@ -1,3 +1,4 @@
+import { createCustomerPortal } from "@/lib/actionsStripe";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
@@ -16,11 +17,12 @@ export default async function PremiumCard() {
         height={150}
         alt=""
         className="rounded-full mb-2"
+        priority
       />
       <h1>Bienvenue {user?.fullName}</h1>
       <p>Email : {user?.emailAddresses[0].emailAddress}</p>
 
-      <form className="my-4" action="">
+      <form className="my-4" action={createCustomerPortal}>
         <button className="p-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full">
           Modifier mon abonnement
         </button>
