525a4491671d1c5a347598e90a74565ef2aa2973
jujudev7 <penna.julien@gmail.com>
2024-11-25 16:20:00 +0100

    layout & home modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-black uppercase">STRIPE NEXT JS 15</h1>
+    <section className="w-full h-[calc(100vh-200px)] flex items-center justify-center">
+      <h1 className="text-5xl font-black uppercase">STRIPE NEXT JS 15</h1>
     </section>
   );
 }
+
